a72798aeea201ef24913c33bab230a8eb595a778
Aamir Khan <amirkhan11999@gmail.com>
2022-08-15 19:55:38 +0500

    changed text color in modal

--- app/src/components/modal.tsx
+++ app/src/components/modal.tsx
@@ -23,7 +23,7 @@ export default ({closeModal}) => {
                     </div> 
                     <div className="flex items-center justify-start p-6 border-t border-solid border-slate-200 rounded-b">
                         <button
-                            className="text-blue-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            className="text-primary background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                             type="button"
                             onClick={closeModal}
                         >
