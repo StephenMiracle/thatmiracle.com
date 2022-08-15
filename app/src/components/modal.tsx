daec0e6f3d6b9e044d88c3c4f0f3dbdcf7aee629
Aamir Khan <amirkhan11999@gmail.com>
2022-08-15 19:42:40 +0500

    changes to modal

--- app/src/components/modal.tsx
+++ app/src/components/modal.tsx
@@ -5,16 +5,16 @@ export default ({closeModal}) => {
 
     return (
         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none" >
-            <div className="relative w-auto my-6 mx-auto max-w-4xl">
+            <div className="relative sm:w-3/5 md:w-3/5 lg:w-/5 xl:w-2/6 2xl:w-1/5 my-6 mx-auto max-w-5xl ">
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                     <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                         <h3 className="text-3xl font-semibold">
                             Greetings
                         </h3>
-                        <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={closeModal}>
+                        <button className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={closeModal}>
                         <span className="sr-only">Close menu</span>
-                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
                     </div>   
